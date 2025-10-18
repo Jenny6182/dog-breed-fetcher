b836090faf8729773733d374640085131e44a37c
KaedeKagami86 <105937682+KaedeKagami86@users.noreply.github.com>
2025-10-17 23:01:34 -0400

    Finished task 1 in DogApiBreedFetcher, but need to change the BreedNotFoundException (task 4) to checked exception

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -9,12 +9,15 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.*;
 
+
 /**
  * BreedFetcher implementation that relies on the dog.ceo API.
  * Note that all failures get reported as BreedNotFoundException
  * exceptions to align with the requirements of the BreedFetcher interface.
  */
 public class DogApiBreedFetcher implements BreedFetcher {
+
+
     private final OkHttpClient client = new OkHttpClient();
 
     /**
@@ -24,12 +27,52 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed){
+        String url_0 = "https://dog.ceo/api/breed/" + breed + "/images"; // change to specific url for that breed
+        // We don't actually need images but just tryna get the error msg by searching for invalid breed
+        Request request0 = new Request.Builder()
+                .url(url_0)
+                .build();
+
+        // Throw BreedNotFoundException if breed doesn't exist
+        try (Response response = client.newCall(request0).execute()) {
+            if (!response.isSuccessful()) {
+                throw new BreedNotFoundException(breed); }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Create an ArrayList to organize all the sub-breeds in
+        ArrayList<String> sub_breeds = new ArrayList<>();
+
+        // Get list of all sub-breeds of the breed
+        String url_1 = "https://dog.ceo/api/breed/" + breed + "/list";
+        Request request1 = new Request.Builder()
+                .url(url_1)
+                .build();
+
+        // Try-catch block
+        try (Response response1 = client.newCall(request1).execute()) {
+            // Parsing API response
+             String response1_body = response1.body().string(); // read ResponseBody as a string
+             JSONObject json = new JSONObject(response1_body); // turn the string into a JSONObject
+             JSONArray msg = json.getJSONArray("message"); // read the msg key-pair JSONArray of msg from the JSONObject
+
+            // loop through the JSONArray and add each String value in the JSONArray into the sub_breeds ArrayList
+            for (int i = 0; i < msg.length(); i++) {
+                sub_breeds.add(msg.getString(i));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return sub_breeds;
+
+
+        // STEPS:
+        // 1. see if breed is valid, if not throw an error
+        // 2. get all sub-breeds of that breed
+        // 3. organize all the sub-breeds in an arraylist
     }
 }
