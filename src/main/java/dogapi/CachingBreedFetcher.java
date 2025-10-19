67bafea3a5e8261ef5acd90494c5dc7958d668bf
KaedeKagami86 <105937682+KaedeKagami86@users.noreply.github.com>
2025-10-18 21:17:39 -0400

    Finished task 2 & task 3.

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -14,15 +14,40 @@ import java.util.*;
  */
 public class CachingBreedFetcher implements BreedFetcher {
     // TODO Task 2: Complete this class
-    private int callsMade = 0;
-    public CachingBreedFetcher(BreedFetcher fetcher) {
 
+    // Store breeds fetched in a map of string key (breed) to list of strings (sub-breeds) (which is ur cache)
+
+    private final BreedFetcher fetcher; // the fetcher we use
+    private final Map<String, List<String>> cache = new HashMap<>();
+    // this stores breeds fetched in a map of string key (breed) to list of strings (sub-breeds) (which is ur cache)
+    private int callsMade = 0; // record calls made to API
+
+    public CachingBreedFetcher(BreedFetcher fetcher) {
+        this.fetcher = fetcher;
+        // means when we try to make a cachingbreedfetcher, we give it a fetcher we defined already
+        // and that defined fetcher will be used as the underlying fetcher
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+        // check if we already had this breed in our cache map
+        if (cache.containsKey(breed)) {
+            return cache.get(breed); // if we do, return the corresponding list, using breed as the key
+        }
+
+        callsMade++; // add to API call count
+
+        try {
+            List<String> subBreeds = fetcher.getSubBreeds(breed);
+            cache.put(breed, subBreeds); // store it in cache
+            return subBreeds;
+        } catch (BreedNotFoundException e) {
+            throw e;
+        }
+
+        // whenever this method is called, if already called before,
+        // cacher should return smth, if not we can ask the website then
+        // If the underlying fetcher throws BreedNotFoundException → don’t cache that.
     }
 
     public int getCallsMade() {
--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -1,5 +1,6 @@
 package dogapi;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -26,6 +27,23 @@ public class Main {
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
         // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
         // return statement included so that the starter code can compile and run.
-        return -1;
+        BreedFetcher fetcher = breedFetcher;
+        List<String> breeds_list = new ArrayList<>();
+
+        int numberOfSubBreeds = 0;
+
+        try {
+            breeds_list = fetcher.getSubBreeds(breed);
+            for (int i = 0; i < breeds_list.size(); i++) {
+                numberOfSubBreeds += 1;
+            }
+        } catch (BreedFetcher.BreedNotFoundException e) {
+            return 0;
+        }
+
+        // if no breed found exception thrown, we count that as 0 sub-breeds
+        // else count the return results' list, just loop throw and see how many subbreeds there are, and return the number
+
+        return numberOfSubBreeds;
     }
 }
